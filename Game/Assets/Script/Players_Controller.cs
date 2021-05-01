84d9ff59840d379506cd299afcd156d71239c334
Michelle <49232352+Mishvva@users.noreply.github.com>
2021-05-01 16:47:47 +1000

    update

---
+++ Game/Assets/Script/Players_Controller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Players_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public float speed; //Player moving speed
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Movement() 
+    }
+
+    void Movement() // Player's movement
+    {
+        float.horizontalmove; // set up variable to save the input
+        horizontalmove = Input.GetAxis("Horizontal"); //Left(-1), Stable(0), Right(1) 
+
+        if(horizontalmove != 0)
+        {
+            rb.volocity = new Vector2(horizontalmove * speed, rb.velocity.y);
+        }
+    
+    }
+}
