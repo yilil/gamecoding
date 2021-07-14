02e3082d6166325a7ee33d1bf4f75827de1d83a6
Yidong Gan <ygan7134@uni.sydney.edu.au>
2021-07-15 01:28:40 +1000

    testing

---
+++ Game/Assets/Script/FacingCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacingCamera : MonoBehaviour
+{
+    Transform[] childs;
+    public bool init = false;
+    void Start()
+    {
+        childs = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            childs[i] = transform.GetChild(i);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < childs.Length; i++)
+        {
+            if(childs[i].name == "house"){
+                if(!init){
+                    init = true;
+                }else{
+                    continue;
+                }
+            }
+            childs[i].rotation = Camera.main.transform.rotation;
+        }
+    }
+}
